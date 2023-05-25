ebf6403098bd457d767764573aa355b37a9fb940
Mayla Bisacchi <Mayla.Bisacchi@sptech.school>
2023-05-25 17:48:19 -0300

    Criação de Assento repository

--- src/main/java/maitre/API/Repository/AssentoRepository.java
+++ src/main/java/maitre/API/Repository/AssentoRepository.java
@@ -1,9 +1,12 @@
 package maitre.API.Repository;
 
 import maitre.API.Entidades.Assento;
+import maitre.API.Entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssentoRepository extends JpaRepository<Assento, Integer> {
 
     Assento findAssentoById(Integer id);
+
+    List<Usuario> find
 }
